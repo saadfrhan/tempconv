e3185849af3a877cf83c461e5e740de48788932c
saadfrhan <saadfarhan347@gmail.com>
2024-12-16 12:01:34 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Temperature Converter
+
+Basic temperature converter that converts between Celsius and Fahrenheit using HTML, CSS and JavaScript.
---
+++ converter.js
@@ -0,0 +1,15 @@
+function convertToFahrenheit() {
+  const celsiusInput = document.getElementById("celsius");
+  const fahrenheitInput = document.getElementById("fahrenheit");
+  const celsius = parseFloat(celsiusInput.value);
+  const fahrenheit = (celsius * 9) / 5 + 32;
+  fahrenheitInput.value = fahrenheit.toFixed(2);
+}
+
+function convertToCelsius() {
+  const celsiusInput = document.getElementById("celsius");
+  const fahrenheitInput = document.getElementById("fahrenheit");
+  const fahrenheit = parseFloat(fahrenheitInput.value);
+  const celsius = ((fahrenheit - 32) * 5) / 9;
+  celsiusInput.value = celsius.toFixed(2);
+}
